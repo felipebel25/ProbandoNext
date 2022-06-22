57da65c628f187a589a88c00bc2da7555abb6304
felipe <felipe@cloud-america.com>
2022-06-21 20:26:38 -0500

    probando next

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar/Navbar'
+import React from 'react'
+
+const about = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <h1>esta es la pagina de About</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar/Navbar'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <h1>Hola platzi!</h1>
+    </div>
+  )
+}
+
+export default Home
