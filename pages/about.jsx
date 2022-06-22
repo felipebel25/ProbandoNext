1ca5ceeb3950fe2c4fa87bbc6a54c02cc822a5bb
felipe <felipe@cloud-america.com>
2022-06-21 20:46:00 -0500

    probando meta etiquetas

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,14 +1,25 @@
-import Navbar from '../components/Navbar/Navbar'
-import React from 'react'
+import Navbar from "../components/Navbar/Navbar";
+import React from "react";
+import Head from "next/head";
 
 const about = () => {
-
   return (
-    <div>
-      <Navbar />
-      <h1>esta es la pagina de About</h1>
-    </div>
-  )
-}
+    <>
+      <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="soy about" />
+        <meta property="og:title" content="titulo"></meta>
+        <meta property="og:description" content="about soy"></meta>
 
-export default about
+
+      </Head>
+        <Navbar />
+        <h1>esta es la pagina de About</h1>
+      </div>
+    </>
+  );
+};
+
+export default about;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import Navbar from '../components/Navbar/Navbar'
 import React from 'react'
+import Head from "next/head";
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
+      <Head>
+        <title>aasdasd</title>
+        <meta name="description" content="soy home" />
+        <meta property="og:description" content="home"></meta>
+        <meta property="og:title" content="titulo"></meta>
+
+      </Head>
         <h1>Hola platzi!</h1>
+        <Navbar />
     </div>
   )
 }
