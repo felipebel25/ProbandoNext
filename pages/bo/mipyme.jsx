dd488e065c1d224bf70ee909357946bd5d7e72fb
felipe <felipe@cloud-america.com>
2022-07-14 10:45:55 -0500

    probando meta tags images

--- pages/bo/mipyme.jsx
+++ pages/bo/mipyme.jsx
@@ -52,7 +52,7 @@ const about = () => {
           <meta content="@sonar_fm" name="twitter:creator" /> */}
           <meta
             name="twitter:image"
-            content="/landing_screenshot.png" 
+            content="https://ibb.co/L62D2p1" 
           />
           <meta
             name="twitter:description"
