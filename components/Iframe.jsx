37e8e7c4fe2c7e4380a923762a7ffa6865289539
felipe <felipe@cloud-america.com>
2022-07-19 21:41:07 -0500

    netlify

---
+++ components/Iframe.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Iframe = () => {
+  return typeof window !== "undefined" ? (
+    <iframe
+      title="goRegister"
+      id="typeform-full"
+      width="100%"
+      height="100%"
+      src='https://dev-go.moyoai.com/Au22BKe5'
+      allow="geolocation; camera"
+    />
+  ) : (
+    "nada"
+  );
+};
+
+export default Iframe;
--- pages/bo/registro-BO.jsx
+++ pages/bo/registro-BO.jsx
@@ -1,34 +1,46 @@
-import React from "react";
+import dynamic from "next/dynamic";
 import Head from "next/head";
+import { useEffect } from "react";
+
+// import Iframe from "../../components/Iframe";
 import RotateScreen from "../../components/RotateScreen";
-import { usePageAnalyitics } from "../../helpers/hooks/usePageAnalytics";
-// import { usePageAnalyitics } from '../helpers/hooks/usePageAnalytics'
+import { getCookies ,setCookie } from 'cookies-next';
+const Iframe = dynamic(() => import("../../components/Iframe"), {
+  ssr: false,
+});
 const RegisterQR = () => {
-  //   usePageAnalyitics()
-  usePageAnalyitics()
+  // usePageAnalyitics();
+  // const domain = () => {
+  //   if (typeof document !== "undefined" && document.domain === "aura.moyoai.com") {
+  //     return  process.env.REACT_APP_URL_REGISTER_PROD
+  //   }else{
+  //     return process.env.REACT_APP_URL_REGISTER_DEV
+  //   }
+  // };
+  // const route = domain()
+  useEffect(() => {
+    setCookie('session', 'uLr5lpIykh76gRYB');
+    console.log( getCookies());
+    
+  }, []);
+   
   return (
     <>
       <Head>
         <title>Register | AURA Finance</title>
       </Head>
-      <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-        <iframe
-          title="goRegister"
-          id="typeform-full"
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          src="https://go.moyoai.com/Au22BKe5"
-          allow="geolocation; camera"
-        />
+      <h1>cambio</h1>
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      {/* <Embed url='https://dev-go.moyoai.com/Au22BKe5' /> */}
+      <Iframe />
       <RotateScreen />
     </>
   );
 };
 
 export default RegisterQR;
-/* banderas como en desktop tablet*/ 
-/* animaciones colores */
-/* titulo en lila mobile */
-/* amarillo tambien en mobile */
-/* router */
